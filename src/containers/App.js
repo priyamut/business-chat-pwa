775b8316291d0ebf49203e4f748d956d27434794
priyamut <priya@agentz.ai>
2019-11-26 18:24:55 +0530

    fixed semicolon

--- src/containers/App.js
+++ src/containers/App.js
@@ -218,7 +218,7 @@ class App extends Component {
               onMessage={(message) => this.onMessageReceive(message)}
               debug={false}
             />
-          )};
+          )}
          
               </div>
             </RTL>
